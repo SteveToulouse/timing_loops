ef45ec594799c2692ffc24840011adda901a7cf4
COMMON Steven IMT/OINIS <USCE5272@orange.com>
2016-06-03 14:04:04 +0200

    add LICENCE and new benchmark source file

---
+++ newbenchmark.ino
@@ -0,0 +1,13 @@
+/*
+  This file will contain a newer, more absolutely fabulous set of timings :-)
+*/
+
+// Basic Arduino "sketch" skeleton code
+
+// Called once, after initial boot or reset
+void setup() {
+}
+
+// Called repeatedly until reset or power down
+void loop() {
+}
